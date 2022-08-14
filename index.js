10046ef2780ffe39b5536124e45883eb2ed73ed7
zakariaahmad <kimo.ahmad2009@gmail.com>
2022-08-14 21:21:54 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "<h1>YOUR-NAME is the champion</h1>";
